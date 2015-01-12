d3381e24ab26aaaa413a45f45f5aeac90ca137ed
Koen van Vliet <13093053@users.noreply.github.com>
2015-01-12 12:38:40 +0100

    Werks

--- pulswidth/pulse_width.c
+++ pulswidth/pulse_width.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <avr/io.h>
 #include "lcd.h"
+#include <util/delay.h>
 
-volatile bool lf = false;
+
+volatile bool lf = true;
 volatile uint16_t pw = 1234;
 //      ____                       _____
 // ____|    |_____________________|     |_____________________|
@@ -13,33 +15,39 @@ volatile uint16_t pw = 1234;
 
 #define ICP PINB0
 
-ISR(TIMER1_COMPA_vect)
+ISR(TIMER1_CAPT_vect)
 {
 	pw=ICR1;
 	TCNT1 = 0;
+	lf = true;
 }
-ISR(__vector_default){}
+//ISR(__vector_default){}
 
 int main()
 {
 	char buffer[17];					// Normal mode, no prescaler
 	TIMSK = 1 << TICIE1;				// Enable input capture interrupts
-	TCCR1B = (1 << ICES1) | (1 << CS10);
+	TCCR1A = 0x00;
+	TCCR1B = ( 1 << ICNC1 ) | (1 << ICES1) | (1 << CS10);
+  	TCNT1 = 0x0000;
 
 	lcd_init();							// Initialize lcd
     lcd_cursor(false, false);			// Cursor off
 
 	sei();
 
-	label:
-	if(lf)
+	while(1)
 	{
-		lcd_home();
-        lcd_puts("Pulsbreedte is:");	// Print pulsewidth
-        lcd_goto(1, 0);
-        snprintf(buffer, sizeof buffer, "%12d us", pw);
-        lcd_puts(buffer);
-		lf = false;					// Clear update flag
+		if(lf)
+		{
+			lcd_home();
+	        lcd_puts("Pulsbreedte is:");	// Print pulsewidth
+	        lcd_goto(1, 0);
+			pw = (uint16_t)(pw / 3.6864);
+	        snprintf(buffer, sizeof buffer, "%12d us", pw);
+	        lcd_puts(buffer);
+			lf = false;					// Clear update flag
+			_delay_ms(1000);
+		}
 	}
-	goto label;
 }
